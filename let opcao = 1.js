aa9101ecc4dc3bda8fbaadc26ebc5b21f96f259a
andergb390 <cursosenact@gmail.com>
2026-03-30 21:35:28 -0300

    faz o L

---
+++ let opcao = 1.js
@@ -0,0 +1,14 @@
+let opcao = 1
+switch (opcao) {
+case 1
+console.log("voce escolheu energetico de tadala");
+brake
+case 2:
+console.log("voce escolheu uma cachaca da boa");
+brake
+case 3:
+console.log(" voce escolheu todynho");
+brake
+default:
+console.log("opcao invalida");
+}
